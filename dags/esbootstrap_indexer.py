e08e5c0a4270da74a0d56d3c2bfc9026e38c07e5
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-06-09 22:41:10 +0300

    Small fixes

--- dags/esbootstrap_indexer.py
+++ dags/esbootstrap_indexer.py
@@ -1,15 +1,21 @@
 from airflow.decorators import dag, task
 from airflow.operators.bash import BashOperator
 from airflow.utils.dates import days_ago
+
 from tasks.debug import debug_value
+
 default_args = {
     "owner": "airflow",
 }
-@task 
+
+
+# TODO: run directly in the bash operator, using Jinja template
+@task
 def build_command(app_name):
-    cmd = 'APP_CONFIG_DIRNAME=%s  elastic_host=elastic /code/app.js create_index' %app_name
+    cmd = 'APP_CONFIG_DIRNAME=%s elastic_host=elastic /code/app.js create_index' % app_name
     print(cmd)
     return cmd
+
 
 @dag(
     default_args=default_args,
@@ -19,11 +25,17 @@ def build_command(app_name):
 )
 def esbootstrap_nodejs_indexer(app_name: str = 'pam'):
     debug_value(app_name)
-    cmd = build_command(app_name)
-    print(cmd)
+    # cmd = build_command(app_name)
+    # print(cmd)
+
+    cmd = 'APP_CONFIG_DIRNAME="{{params.app_name}}" '\
+        'elastic_host=elastic /code/app.js create_index'
+
     esbootstrap_indexer_task = BashOperator(
         task_id="bash_task",
         bash_command=cmd,
+        params={app_name: app_name}
     )
+
 
 esbootstrap_nodejs_indexer_dag = esbootstrap_nodejs_indexer()
