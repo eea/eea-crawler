73a82e8e6401e19b79e3247abf0e3d3381a16b29
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2021-12-01 16:26:11 +0200

    update

--- dags/nlp_1_prepare_docs_for_nlp_from_es.py
+++ dags/nlp_1_prepare_docs_for_nlp_from_es.py
@@ -20,7 +20,7 @@ default_args = {"owner": "airflow"}
 default_dag_params = {
     "item": "http://www.eea.europa.eu/api/@search?portal_type=Highlight&sort_order=reverse&sort_on=Date&created.query=2021/6/1&created.range=min&b_size=500",
     "params": {
-        "fast": False,
+        "fast": True,
         "portal_type": "",
         "site": "",
     },
