b13f0d4a871ec74ecbf7a32e8484f5293007ecc5
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2021-12-08 15:58:14 +0200

    bise normalization

--- dags/normalizers/sites/site_biodiversity.py
+++ dags/normalizers/sites/site_biodiversity.py
@@ -4,15 +4,60 @@ from normalizers.registry import (
     register_facets_normalizer,
     register_nlp_preprocessor,
 )
-from normalizers.lib.normalizers import common_normalizer
+from normalizers.lib.normalizers import (
+    common_normalizer,
+    check_blacklist_whitelist,
+    find_ct_by_rules,
+)
 from normalizers.lib.nlp import common_preprocess
+import logging
+
+logger = logging.getLogger(__file__)
 
 
 @register_facets_normalizer("biodiversity.europa.eu")
 def normalize_biodiversity_europa_eu(doc, config):
-    normalized_doc = common_normalizer(doc, config)
+    logger.info("NORMALIZE BISE")
+    logger.info(doc["raw_value"]["@id"])
+    logger.info(doc["raw_value"]["@type"])
+    logger.info(doc)
+    ct_normalize_config = config["site"].get("normalize", {})
 
+    if not check_blacklist_whitelist(
+        doc,
+        ct_normalize_config.get("blacklist", []),
+        ct_normalize_config.get("whitelist", []),
+    ):
+        logger.info("blacklisted")
+        return None
+    logger.info("whitelisted")
+
+    if doc["raw_value"]["@type"] == "File":
+        if doc["raw_value"]["file"]["content-type"] != "application/pdf":
+            logger.info("file, but not pdf")
+            return None
+    if doc["raw_value"]["@type"] == "bise_factsheet":
+        doc["raw_value"]["spatial"] = doc["raw_value"]["title"]
+
+    normalized_doc = common_normalizer(doc, config)
+    logger.info("TYPES:")
+    logger.info(normalized_doc["objectProvides"])
+    if normalized_doc["objectProvides"] == "Webpage":
+        logger.info("CHECK LOCATION:")
+        doc_loc = urlparse(normalized_doc["id"]).path
+        logger.info(doc_loc)
+        ct = find_ct_by_rules(
+            doc_loc,
+            ct_normalize_config.get("location_rules", []),
+            ct_normalize_config.get("location_rules_fallback", "Webpage"),
+        )
+        logger.info(ct)
+        normalized_doc["objectProvides"] = ct
+    if normalized_doc["objectProvides"] == "Country fact sheet":
+        normalized_doc["objectProvides"] = ["Country fact sheet", "Dashboard"]
+    logger.info(normalized_doc["objectProvides"])
     normalized_doc["cluster_name"] = "BISE (biodiversity.europa.eu)"
+    normalized_doc["topic"] = "Biodiversity — Ecosystems"
 
     return normalized_doc
 
