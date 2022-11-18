0f9e30a4af58ff4e16ca63b197dee3e56ee228a5
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2022-11-18 13:48:53 +0200

    added new dag to fetch obligations & instruments

---
+++ dags/d0_update_obligations.py
@@ -0,0 +1,67 @@
+from airflow.decorators import dag, task
+from airflow.models import Variable
+from airflow.utils.dates import days_ago
+from lib.dagrun import trigger_dag
+
+from airflow.models import Variable
+from rdflib import Graph
+
+import json
+
+default_args = {"owner": "airflow"}
+
+
+@task
+def updateNormObj():
+    g = Graph()
+    g.parse("https://rod.eionet.europa.eu/obligations/rdf")
+    g.parse("https://rod.eionet.europa.eu/instruments/rdf")
+
+    results = g.query(
+        """
+        SELECT ?obl ?instr ?instr_label ?instr_identifier
+        WHERE {
+            ?obl a <http://rod.eionet.europa.eu/schema.rdf#Obligation> .
+            ?instr a <http://rod.eionet.europa.eu/schema.rdf#Instrument> .
+            ?obl <http://rod.eionet.europa.eu/schema.rdf#instrument> ?obl_instr .
+            OPTIONAL {?instr <http://www.w3.org/2000/01/rdf-schema#label> ?instr_label} .
+            OPTIONAL {?instr <http://purl.org/dc/terms/identifier> ?instr_identifier} .
+            FILTER (?instr = ?obl_instr)
+        }
+        """
+    )
+    obligations = {}
+    for (obl, instr, label, identifier) in results:
+        obligations[obl.toPython()] = {
+            "label": label.toPython(),
+            "instrument": instr.toPython(),
+        }
+    Variable.set(
+        "obligations",
+        json.dumps(obligations),
+        description="generated variable, don't modify, it will be overwritten",
+    )
+
+
+@task
+def test_variable():
+    obl = Variable.get("obligations", deserialize_json=True)
+    print(obl)
+    print(obl["http://rod.eionet.europa.eu/obligations/104"])
+    print(obl.get("http://rod.eionet.europa.eu/obligations/8"))
+
+
+@dag(
+    default_args=default_args,
+    start_date=days_ago(2),
+    schedule_interval=None,
+    description="test",
+    tags=["test"],
+)
+def d0_update_obligations():
+    t1 = updateNormObj()
+    t2 = test_variable()
+    t1 >> t2
+
+
+update_obligations_dag = d0_update_obligations()
