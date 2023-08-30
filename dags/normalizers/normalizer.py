a38deeecba20ccd30179f61499059ea5ed31d39e
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2023-08-30 16:16:17 +0300

    added possibility to only get the documents for a specific site

--- dags/normalizers/normalizer.py
+++ dags/normalizers/normalizer.py
@@ -2,6 +2,18 @@ from lib import elastic
 from normalizers.registry import get_facets_normalizer, get_nlp_preprocessor
 from normalizers.lib.nlp import preprocess_split_doc, add_embeddings_to_doc
 
+def parse_all_documents_for_site(site_id, v, handler, doc_handler):
+    print("HERE")
+    print(site_id)
+    raw_docs = elastic.get_all_ids_from_raw_for_site(v, site_id)
+    for raw_doc in raw_docs.keys():
+        should_index = True
+        if len(raw_docs[raw_doc].get("errors")) > 0:
+            should_index = False
+        if should_index:
+            #print(raw_docs[raw_doc])
+#            if raw_doc == 'https://water.europa.eu/freshwater/nwrm-imported/measures/buffer-strips-and-hedges':
+            handler(v, raw_doc, site_id, doc_handler)
 
 def parse_all_documents(v, handler, doc_handler):
     raw_docs = elastic.get_all_ids_from_raw(v)
@@ -34,6 +46,10 @@ def preprocess_doc(v, doc_id, site_id, raw_doc, doc_handler):
     site_config_variable = sites.get(site_id, None)
     site_config = v.get(site_config_variable)
 
+
+    print(sites)
+    print(site_config_variable)
+    print(site_config)
     normalizers_config = v.get(
         site_config.get("normalizers_variable", "default_normalizers")
     )
