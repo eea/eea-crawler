c4644f052b1ec75a408ee677c5c6d5fe1a289e0b
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2023-09-14 15:35:14 +0300

    updated indexing for wise marine

--- dags/normalizers/sites/site_wise_marine.py
+++ dags/normalizers/sites/site_wise_marine.py
@@ -16,6 +16,55 @@ import logging
 logger = logging.getLogger(__file__)
 
 
+wm_spm_extra_fields = ["title",
+    "sector",
+    "code",
+    "use",
+    "origin",
+    "nature",
+    "status",
+    "impacts",
+    "impacts_further_details",
+    "water_body_cat",
+    "spatial_scope",
+    "country_coverage",
+    "measure_purpose",
+    "measure_type",
+    "measure_location",
+    "measure_response",
+    "measure_additional_info",
+    "pressure_type",
+    "pressure_name",
+    "ranking",
+    "season",
+    "approaching_areas",
+    "areas_to_be_avoided",
+    "descriptors",
+    "ecological_impacts",
+    "future_scenarios",
+    "effect_on_hydromorphology",
+    "ktms_it_links_to",
+    "links_to_existing_policies",
+    "msfd_spatial_scope",
+    "mspd_implementation_status",
+    "nature_of_physical_modification",
+    "source",
+    "authority",
+    "general_view",
+    "ports",
+    "future_expectations",
+    "safety_manner",
+    "objective",
+    "categories",
+    "precautionary_areas",
+    "priority_areas",
+    "relevant_targets",
+    "relevant_features_from_msfd_annex_iii",
+    "region",
+    "shipping_tackled",
+    "traffic_separation_scheme",
+    "type_of_pressure"]
+
 @register_facets_normalizer("wise_marine")
 def normalize_marine(doc, config):
     logger.info("NORMALIZE MARINE")
@@ -63,6 +112,13 @@ def normalize_marine(doc, config):
             normalized_doc["objectProvides"] = ["Webpage"]
         else:
             normalized_doc["objectProvides"].remove("Webpage")
+    if "Shipping and Ports Measure" in normalized_doc["objectProvides"]:
+        normalized_doc['exclude_from_globalsearch'] = ['True']
+        if normalized_doc.get("issued") is None and normalized_doc.get("hasWorkflowState") == "published":
+            normalized_doc["issued"] = "2023-09-04T07:17:00"
+        for extra_field in wm_spm_extra_fields:
+            normalized_doc[f'wm_spm_{extra_field}'] = doc["raw_value"].get(extra_field)
+
     print("OBJECT PROVIDES")
     print(normalized_doc["objectProvides"])
 
